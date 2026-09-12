3d1704ebbe6ffa9e42302c5e34ece0066d627643
keenl3 <queenlyjoyromanos386@gmai.com>
2026-09-12 12:03:53 +0800

    Fixed errors from last laboratory activity

--- quiz_app/lib/change_color.dart
+++ quiz_app/lib/change_color.dart
@@ -2,43 +2,62 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 
-class ChangeColor extends StatefulWidget(){
+class ChangeColor extends StatefulWidget {
   const ChangeColor({super.key});
+
 @override
   State<ChangeColor> createState(){
     return _ChangeColorState();
+
   }
 }
 
 
+
 class _ChangeColorState extends State<ChangeColor>{
   final randomizer = Random();
-  var colors = (Colors.orange, Colors.deepPurple, Colors.green),
+
+
+  var colors = [const Color.fromARGB(255, 251, 127, 168), Colors.deepPurple, Colors.blue];
   int select = 0;
+
   void start(){
     setState(() {
       select = randomizer.nextInt(colors.length);
     });
-  }
+}
 
 
   @override
-  Widget build(context){
+  Widget build(BuildContext context){
     return Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('asset/logo.png', color: colors.elementAt(select),),
-          Text('Learn Flutter in a fun way!',
-          style: TextStyle(color: colors.elementAt(select), fontSize: 30),),
+          Image.asset(
+          'asset/logo.png', 
+          color: colors.elementAt(select),
+          ),
+
+          Text(
+            'Learn Flutter in a fun way!',
+          style: TextStyle(
+            color: colors.elementAt(select), 
+            fontSize: 30
+            ),
+          ),
+
           OutlinedButton(
             onPressed: start,
-            child: Text('Start Quiz'),
-            style: TextStyle(color: colors.elementAt(select), fontSize: 30),
-          
-          )
-        ]
+            style: OutlinedButton.styleFrom(side: BorderSide(color: colors.elementAt(select)),
+            textStyle: TextStyle(fontSize: 30),
+            ),
+
+            child: Text(
+              'Start Quiz',
+              style: TextStyle(color: colors.elementAt(select)),
+          ),
+        ),
+      ]
     );
-  
-    
-    
-  }
+  }  
+}
--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/change_color.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(
-    backgroundColor: Colors.pink,
-    body: Center())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 230, 34, 100),
+          ),
+          child: Center(
+            child: const ChangeColor(),
+          ),
+        ),
+      ),
+    )
+  );
 }
+  
