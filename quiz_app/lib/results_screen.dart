8c9478bbb02c9ecd474983dac0bddb3d6b3bba4a
keenl3 <queenlyjoyromanos386@gmai.com>
2026-09-13 18:36:38 +0800

    Connect restart function to the results screen

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -3,9 +3,10 @@ import 'package:quiz_app/data/question.dart';
 
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen({super.key, required this.chosenAnswers, required this.onRestart});
 
   final List<String> chosenAnswers;
+  final void Function() onRestart;
  
   List<Map<String, Object>> getSummaryData(){
     final List<Map<String, Object>> summary = [];
@@ -54,7 +55,7 @@ class ResultsScreen extends StatelessWidget {
               ),
               const SizedBox(height: 30),
                 TextButton.icon(
-                  onPressed: () {},
+                  onPressed: onRestart,
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.white,
                     side: const BorderSide(color: Color.fromARGB(255, 90, 35, 58)),
