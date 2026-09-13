8bbc82e77ba2ba795a2cb458450aab06e5e88e46
keenl3 <queenlyjoyromanos386@gmai.com>
2026-09-13 15:48:58 +0800

    Create basic AnswerButton class structure and constructor 2

--- quiz_app/lib/answer_button.dart
+++ quiz_app/lib/answer_button.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({super.key, required this.answerText, required this.onTap});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(answerText);
+  }
+}
