628385c1cf801dac5acfa47bfe5c7bc672919cd2
keenl3 <queenlyjoyromanos386@gmai.com>
2026-09-15 12:57:25 +0800

    Fix the main file

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -2,17 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 230, 34, 100),
-          ),
-          child: const Quiz(),
-        ),
-      ),
-    )
-  );
+  runApp(const Quiz());
 }
   
